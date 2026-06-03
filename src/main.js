531d43f0c6e51465a1f962a9e5c1d0cd8de528c4
Angelina <bogdanangelina@gmail.com>
2026-06-03 16:25:06 +0300

    create project

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/header';
+import './js/hero';
+import './js/about';
+import './js/how-to-play';
+import './js/features';
+import './js/faq';
+import './js/cta';
+import './js/footer';
+import './js/cookies';
